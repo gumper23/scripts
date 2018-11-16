d8f459b90b78460fd9507b2e3e0a584ef0ea3001
Russ Smith <gumper@gumper.com>
2018-11-16 05:07:59 -0800

    Targeted the stopping of containers to horseshoe containers.

--- bash/stop_horseshoe.sh
+++ bash/stop_horseshoe.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
-for p in 43306 33306 23306 13306; do
-    mysqladmin -h 127.0.0.1 -P${p} shutdown;
+for c in $(docker ps | tail -n+2 | awk '{print $NF}' | grep -P 'mysql[0-9]+'); do
+    p=$(docker inspect "${c}" | grep MYSQL_PORT | grep -o '[0-9]*');
+    mysqladmin -h 127.0.0.1 -P"${p}" shutdown;
 done
 
 # shellcheck disable=SC2034
 for i in {1..60}; do 
-    if [ "$(docker ps | wc -l)" -eq 1 ]; then
+    if [ "$(docker ps | tail -n+2 | awk '{print $NF}' | grep -c -P 'mysql[0-9]+')" -eq 0 ]; then
         break;
-    fi;
+    fi; 
     sleep 1;
 done
 
-if [ "$(docker ps | wc -l)" -ne 1 ]; then
+if [ "$(docker ps | tail -n+2 | awk '{print $NF}' | grep -c -P 'mysql[0-9]+')" -ne 0 ]; then
+    echo "Unable to stop all containers";
     exit 1;
 fi
+
 
