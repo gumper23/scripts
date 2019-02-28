4eb60012bcc29b06f9a7682b9bebca0757c2d068
Russ Smith <gumper@gumper.com>
2019-02-28 05:10:12 -0800

    Added mysqlx support; fixed broken upgrade_horseshoe.sh

--- bash/upgrade_horseshoe.sh
+++ bash/upgrade_horseshoe.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-docker pull mysql:latest
+# Stop/remove any running mysql containers
 ./stop_horseshoe.sh
 for c in $(docker ps -a | awk '{print $NF}' | grep -P 'mysql[0-9]+'); do
     docker rm "${c}";
 done
+
+# Delete any mysql images
+for i in $(docker images | grep mysql | awk '{print $3}' | sort | uniq); do
+    docker rmi -f "${i}";
+done
+
+docker pull mysql:latest
 ./make_horseshoe.sh
 
 for c in $(docker ps -a | awk '{print $NF}' | grep -P 'mysql[0-9]+'); do
